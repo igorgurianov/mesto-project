134cedf8207be0ab7dbb92efbf96980f78bbbf28
Igor Gurianov <igor.gurianov@inbox.ru>
2022-11-19 19:27:11 +0500

    corrected review notes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const addCardPopup = document.querySelector('.popup_type_add_card') // выбираем куда будем добавлять класс opened
 const addCardButton = document.querySelector('.profile__add-button') // выбираем в ДОМ кнопку открытия попапа
 const addCardPopupContainer = addCardPopup.querySelector('.popup__container') // выбираем контейнер в попапе с новыми карточками
-const CloseBttnAddCardPopup = addCardPopupContainer.querySelector('.popup__close-button') // выбираем кнопку в контейнере в попапе с новыми карточками
+const closeBttnAddCardPopup = addCardPopupContainer.querySelector('.popup__close-button') // выбираем кнопку в контейнере в попапе с новыми карточками
 
 // Попап редактирования профиля
 const profileEditButton = document.querySelector('.profile__edit-button')
@@ -19,21 +19,37 @@ const largeImage = document.querySelector('.popup__image');
 const cardsContainer = document.querySelector('.cards-grid');
 const userTemplate = document.querySelector('#card-template').content; // выбираем темплейт
 const formElement = document.querySelector('form', '.popup__input-container_type_profile')
+
+// Редактирования профиля
 const nameInput = formElement.querySelector('.popup__text-input_type_name')
 const jobInput = formElement.querySelector('.popup__text-input_type_description')
 const profileName = document.querySelector('.profile__name')
 const profileJob = document.querySelector('.profile__description')
 
-// добавление новых карточек
-function addCard(cardNameValue, cardSrcValue) {
+// Кнопка добавить место
+const addCardForm = document.querySelector('.popup__input-container_type_add'); // выбираем форму контейнера
+const cardName = document.querySelector('.popup__text-input_type_card-name'); // выбираем инпут для названия карточки
+const cardSrc = document.querySelector('.popup__text-input_type_link') // выбираем инпут для линка картинки
+
+// Поиск всех крестиков (кнопок закрытия)
+const closeButtons = document.querySelectorAll('.popup__close-button')
+
+// создание новых карточек
+function createCard(cardNameValue, cardSrcValue) {
   const cardElement = userTemplate.querySelector('.place').cloneNode(true); //клонируем див внутри него со всем содержимым
   cardElement.querySelector('.place__name').textContent = cardNameValue; //подставляем название места из инпута
   cardElement.querySelector('.place__photo').src = cardSrcValue; // подставляем ссылку из инпута
   cardElement.querySelector('.place__photo').alt = cardNameValue
-  cardElement.querySelector('.place__photo').addEventListener('click', function (event) {clickLargeImage (event,cardNameValue)}) // слушатель на картинку для открытия попапа - передаем ему 2 аргумента
+  cardElement.querySelector('.place__photo').addEventListener('click', function (event) { clickLargeImage(event, cardNameValue) }) // слушатель на картинку для открытия попапа - передаем ему 2 аргумента
   cardElement.querySelector('.place__like-button').addEventListener('click', clickLikeCard) // Слушатель на кнопку лайк
   cardElement.querySelector('.place__delete-button').addEventListener('click', clickDeleteCard) // слушатель на кнопку удаления карточки
-  cardsContainer.prepend(cardElement); // вставляем наш контейнер с названием и ссылкой в начало блока карточек
+  return cardElement
+}
+
+// отображение новых карточек в DOM
+function renderCard(card) {
+  cardsContainer.prepend(card); // вставляем наш контейнер с названием и ссылкой в начало блока карточек
+  addCardForm.reset()
 }
 
 // Функция коллбэк для слушателя на кнопке удалить.
@@ -76,29 +92,20 @@ const initialCards = [
 ];
 
 initialCards.forEach(function (item) {
-  addCard(item.name, item.link)
+  renderCard(createCard(item.name, item.link))
 })
 
 // Функция открытия попапа - переиспользуется для открытия попапов редактирования и добавления, получая нужный аргумент
-function openPopup (popupElement) {
+function openPopup(popupElement) {
   popupElement.classList.add('popup_opened')
 }
 
-// Функция закрытия попапа
-function closePopup (popupElement) {
-  popupElement.classList.remove('popup_opened')
-}
-
-// ПОПАП РЕДАКТИРОВАНИЯ
-
-//  Открыть / закрыть попап редактирования
+//  Открыть попап редактирования
 profileEditButton.addEventListener('click', function () {
   openPopup(editProfilePopup)
+  nameInput.value = profileName.textContent
+  jobInput.value = profileJob.textContent
 });
-
-popupCloseButton.addEventListener('click', function() {
-  closePopup(editProfilePopup)
-})
 
 // Кнопка сохранить
 function handleFormSubmit(evt) {
@@ -110,42 +117,35 @@ function handleFormSubmit(evt) {
 
 formElement.addEventListener('submit', handleFormSubmit); // сабмит слушатель на саму форму
 
-// ПОПАП ДОБАВЛЕНИЯ карточек
-//   Открыть/закрыть попап добавления
+// Открыть попап добавления карточек
 addCardButton.addEventListener('click', function () {
   openPopup(addCardPopup)
 })
 
-CloseBttnAddCardPopup.addEventListener('click', function() {
-  closePopup(addCardPopup)
-})
-
-//   Кнопка "Создать" - добавить место
-const addCardForm = document.querySelector('.popup__input-container_type_add'); // выбираем форму контейнера
-
-function click(evt) {
+// Кнопка "Создать" - добавить место
+function handleCardFormSubmit(evt) {
   evt.preventDefault();
-  const cardName = addCardForm.querySelector('.popup__text-input_type_name'); // выбираем инпут для названия карточки
-  const cardSrc = addCardForm.querySelector('.popup__text-input_type_link') // выбираем инпут для линка картинки
-  console.log(` ${cardName.value} ${cardSrc.value}`);
-  addCard(cardName.value, cardSrc.value)
-  closePopup(addCardPopup)
-  cardName.value = '';
-  cardSrc.value = ''
+  renderCard(createCard(cardName.value, cardSrc.value));
+  closePopup(addCardPopup);
 }
 
-addCardForm.addEventListener('submit', click) // сабмит слушатель на саму форму
+addCardForm.addEventListener('submit', handleCardFormSubmit) // сабмит слушатель на саму форму
 
-// ПОПАП С КАРТИНКОЙ
-//    Закрыть попап
-closeBtnImagePopup.addEventListener('click', function(){
-  closePopup(imagePopupForm)
-})
-
-//    Открыть попап
-function clickLargeImage (event, cardNameValue) {
+// Открыть попап с картинкой
+function clickLargeImage(event, cardNameValue) {
   openPopup(imagePopupForm);
   largeImage.src = event.target.src;
   largeImage.alt = cardNameValue;
   popupLabel.textContent = cardNameValue
+}
+
+// Закрытие всех попапов
+
+closeButtons.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => { closePopup(popup) })
+})
+
+function closePopup(popupElement) {
+  popupElement.classList.remove('popup_opened')
 }
